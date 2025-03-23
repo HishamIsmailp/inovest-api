50c7d80347e4ff3402b551c6f8744c46964fdba2
hisham <hisham@kiebot.com>
2025-03-23 17:20:03 +0530

    change avatar generate api

--- src/utils/avatarUtils.ts
+++ src/utils/avatarUtils.ts
@@ -1,4 +1,5 @@
 export function generateAvatarUrl(name: string): string {
   const username = name.split(" ").join("+");
-  return `https://avatar.iran.liara.run/username?username=${username}`;
+  // return `https://avatar.iran.liara.run/username?username=${username}`;
+  return `https://ui-avatars.com/api/?name=${username}`;
 }
